a9a172dbbd156665d8f964a0241346bcb9e7b506
Fredy Coronado <ing.ambfredycoronado@gmail.com>
2020-10-14 10:41:51 -0500

    se inicia el proyecto

---
+++ database/factories/ConductorFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\conductor;
+use Faker\Generator as Faker;
+
+$factory->define(conductor::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/VehiculoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\vehiculo;
+use Faker\Generator as Faker;
+
+$factory->define(vehiculo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
